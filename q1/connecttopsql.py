6000202f494664e9ea3f8c01dcfbb0b9c196cfb3
mac <mac.ma@connect.ust.hk>
2017-12-01 07:05:16 +0000

    update connect string and print timer

--- q1/connecttopsql.py
+++ q1/connecttopsql.py
@@ -5,6 +5,7 @@ import pprint
 import numpy as np
 import svm as svm
 import os
+import datetime
 
 totalVideoLength = 19228.14
 
@@ -20,8 +21,8 @@ def createFeatures(records, istrain):
 	cacheSessionId = ''
 	cacheVideoId = ''
 	# 63 videos
-	print(timedelta_total_seconds(records[5][9] - records[3][9]))  # /135.95
-	print((records[5][9] - records[3][9]))  # /135.95
+	# print(timedelta_total_seconds(records[5][9] - records[3][9]))  # /135.95
+	# print((records[5][9] - records[3][9]))  # /135.95
 	# for i in range(len(records)):
 
 	f = np.zeros(64)  # 63videos, 1 total second, 1 total session, 1
@@ -54,6 +55,7 @@ def createFeatures(records, istrain):
 			featurelist.append(map(lambda x: float(x), f))
 	print 'features count: ', len(featurelist)
 	if(istrain):
+		print featurelist[0]
 		return {"featureList":featurelist,"labelList":labellist}
 	else:
 		return {"featureList":featurelist, "userList":userlist}
@@ -62,8 +64,9 @@ def createFeatures(records, istrain):
 
 
 def main():
+	print 'program start:', datetime.datetime.now()
 	#Define our connection string
-	conn_string = "host='localhost' dbname='sammy' user='sammy' password='sammy'"
+	conn_string = "host='52.74.79.13' dbname='sammy' user='sammy' password='sammy'"
 
 	# print the connection string we will use to connect
 	print "Connecting to database\n	->%s" % (conn_string)
@@ -84,7 +87,7 @@ def main():
 
 	# where tf.user_id in ('ff930d24cbdeb11e6dde8ceb0da5ac64', 'eee1df0fff33a37873990992bed20e82') \
 	records = cursor.fetchall()
-	print('fetch train data done')
+	print('fetch train data done, ', datetime.datetime.now())
 	svm_trainset = createFeatures(records, True)
 
 
@@ -93,10 +96,12 @@ def main():
 	 order by user_id, event_time;")
 	# where tf.user_id in ('a74fe6d4812fa93a1afa1a6a334ebdda', '4ab9d6eadf7510198f468d10fc29f689', '55654c092cd47b64ec9860f6a9cf3b40') \
 	records = cursor.fetchall()
+	print('fetch test data done, ', datetime.datetime.now())
 	svm_testset = createFeatures(records, False)
 
 	svm.train(svm_trainset['featureList'], svm_trainset['labelList'])
 	svm.classify(svm_testset['featureList'], svm_testset['userList'])
 
+	print('program finish', datetime.datetime.now())
 if __name__ == "__main__":
 	main()
